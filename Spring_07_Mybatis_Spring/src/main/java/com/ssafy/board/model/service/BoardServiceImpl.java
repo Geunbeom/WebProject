988bc56c5a7e106a395b30287539ceafe36f1837
Geunbeom <160552393+Geunbeom@users.noreply.github.com>
2024-04-09 11:34:34 +0900

    Add files via upload

---
+++ Spring_07_Mybatis_Spring/src/main/java/com/ssafy/board/model/service/BoardServiceImpl.java
@@ -0,0 +1,45 @@
+package com.ssafy.board.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.board.model.dao.BoardDao;
+import com.ssafy.board.model.dto.Board;
+
+@Service
+public class BoardServiceImpl implements BoardService{
+	private final BoardDao boardDao;
+	@Autowired
+	public BoardServiceImpl(BoardDao boardDao) {
+		this.boardDao = boardDao;
+	}
+	
+	@Override
+	public List<Board> getBoardList() {
+		return boardDao.selectAll();
+	}
+
+	@Override
+	public Board readBoard(int id) {
+		boardDao.updateViewCnt(id);
+		return boardDao.selectOne(id);
+	}
+
+	@Override
+	public void writeBoard(Board board) {
+		boardDao.insertBoard(board);
+	}
+
+	@Override
+	public void removeBoard(int id) {
+		boardDao.deleteBoard(id);
+	}
+
+	@Override
+	public void modifyBoard(Board board) {
+		boardDao.updateBoard(board);
+	}
+
+}
